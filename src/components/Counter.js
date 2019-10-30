2e0fd7ada9dff8f77bf172812adb6ea182935658
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-10-30 16:16:36 +0700

    milestone-6

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Counter() {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count+1)
+    }
+
     return (
         <div>
-            <h1>H</h1>
-            <button>Increase</button>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increase</button>
         </div>
     )
 }
+
+
--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme';
+import { shallow, mount} from 'enzyme';
 import Counter from './Counter'
 import configureMockStore from 'redux-mock-store';
 import { incrementCounter } from '../actions/actions';
 import { counterReducer } from '../reducers/reducers'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from '../App'
 
 
 
@@ -48,3 +51,18 @@ beforeEach(() => {
   });
 
 })
+
+describe('Counter integration test', () => {
+    let store;
+  
+    beforeEach(() => {
+      store = createStore(counterReducer);
+    });
+  
+    it('increments the counter text when the button is clicked', () => {
+      const wrapper = mount(<Provider store={store}><App /></Provider>);
+      wrapper.find('button').simulate('click');
+      wrapper.update();
+      expect(wrapper.find('h1').text()).toEqual('1');
+    })
+  })

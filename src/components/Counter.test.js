859ea8ec278cb76a46ff686ab4f79662aa99af3b
Hang Yoko Tran <hihihehe0321@gmail.com>
2019-10-30 14:48:13 +0700

    milestone-4

--- src/components/Counter.test.js
+++ src/components/Counter.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import Counter from './Counter'
+import configureMockStore from 'redux-mock-store';
+import { incrementCounter } from '../actions/actions';
 
 
 
@@ -15,5 +17,23 @@ it('renders button components', () => {
     const wrapper = shallow(<Counter />);
     expect(wrapper.contains(<button>Increase</button>)).toEqual(true)
 })
+
+
+})
+
+describe('Counter redux pieces', () => {
+    let store
+beforeEach(() => {
+    const mockStore = configureMockStore();
+    store = mockStore({});
+  });
+
+  it('sends an increment counter action', () => {
+    store.dispatch(incrementCounter());
+    expect(store.getActions()).toEqual(
+      [
+        { type: 'INCREMENT_COUNTER' },
+      ]);
+  })
 
 })
